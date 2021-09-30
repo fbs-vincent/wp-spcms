eecfae239e6626e1c8176788fb92a1d5bed56c5c
Vincent Ramirez <vincent.ramirez@frontlinebusiness.com.ph>
2021-10-01 07:33:54 +0800

    First Commit

---
+++ template/template-partners.php
@@ -0,0 +1,29 @@
+<section class="partners">
+      <div class="container">
+        <div class="section-title">
+          <h2><span>Our</span>Partners</h2>
+        </div>
+
+        <ul>
+          <li>
+            <img src="https://spcms.frontlinebusiness.com.ph/wp-content/uploads/2021/09/who.png" alt="" />
+          </li>
+
+          <li>
+            <img src="https://spcms.frontlinebusiness.com.ph/wp-content/uploads/2021/09/doh.png" alt="" />
+          </li>
+
+          <li>
+            <img src="https://spcms.frontlinebusiness.com.ph/wp-content/uploads/2021/09/prc.png" alt="" />
+          </li>
+
+          <li>
+            <img src="https://spcms.frontlinebusiness.com.ph/wp-content/uploads/2021/09/phil.png" alt="" />
+          </li>
+
+          <li>
+            <img src="https://spcms.frontlinebusiness.com.ph/wp-content/uploads/2021/09/pafp.png" alt="" />
+          </li>
+        </ul>
+      </div>
+    </section>
