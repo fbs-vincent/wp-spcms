e53a9255f75e288b8eaae5280b2c32290a6bbb00
Vincent Ramirez <vincent.ramirez@frontlinebusiness.com.ph>
2021-10-04 13:17:36 +0800

    Updates

--- template/template-partners.php
+++ template/template-partners.php
@@ -5,25 +5,27 @@
         </div>
 
         <ul>
-          <li>
-            <img src="https://spcms.frontlinebusiness.com.ph/wp-content/uploads/2021/09/who.png" alt="" />
-          </li>
+          <div class="grid-col-5">
+            <li>
+              <img src="https://spcms.frontlinebusiness.com.ph/wp-content/uploads/2021/09/who.png" alt="" />
+            </li>
 
-          <li>
-            <img src="https://spcms.frontlinebusiness.com.ph/wp-content/uploads/2021/09/doh.png" alt="" />
-          </li>
+            <li>
+              <img src="https://spcms.frontlinebusiness.com.ph/wp-content/uploads/2021/09/doh.png" alt="" />
+            </li>
 
-          <li>
-            <img src="https://spcms.frontlinebusiness.com.ph/wp-content/uploads/2021/09/prc.png" alt="" />
-          </li>
+            <li>
+              <img src="https://spcms.frontlinebusiness.com.ph/wp-content/uploads/2021/09/prc.png" alt="" />
+            </li>
 
-          <li>
-            <img src="https://spcms.frontlinebusiness.com.ph/wp-content/uploads/2021/09/phil.png" alt="" />
-          </li>
+            <li>
+              <img src="https://spcms.frontlinebusiness.com.ph/wp-content/uploads/2021/09/phil.png" alt="" />
+            </li>
 
-          <li>
-            <img src="https://spcms.frontlinebusiness.com.ph/wp-content/uploads/2021/09/pafp.png" alt="" />
-          </li>
+            <li>
+              <img src="https://spcms.frontlinebusiness.com.ph/wp-content/uploads/2021/09/pafp.png" alt="" />
+            </li>
+          </div>
         </ul>
       </div>
     </section>
